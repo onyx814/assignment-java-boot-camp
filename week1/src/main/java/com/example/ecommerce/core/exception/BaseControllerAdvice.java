95297e7110e11cb67dac7ff60864676819f20e02
onyx814 <love.onyx814@gmail.com>
2022-02-25 20:54:57 +0700

    feat: เพิ่ม ExceptionHandler "internal server error" #9

--- week1/src/main/java/com/example/ecommerce/core/exception/BaseControllerAdvice.java
+++ week1/src/main/java/com/example/ecommerce/core/exception/BaseControllerAdvice.java
@@ -5,10 +5,16 @@ import com.example.ecommerce.core.model.ResponseHeader;
 import com.example.ecommerce.core.model.ResponseObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class BaseControllerAdvice {
@@ -23,4 +29,14 @@ public class BaseControllerAdvice {
     public ResponseObject notFoundException(BaseNotFoundException e){
         return  new ResponseObject(this.initHeaderFail(HttpStatus.NOT_FOUND.value(),e.getMessage()+" "+HttpStatus.NOT_FOUND.getReasonPhrase()+ "!"), null);
     }
+
+    @ExceptionHandler({Exception.class})
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseObject handleException(Exception e){
+        Object ex = e;
+        return  new ResponseObject(this.initHeaderFail(HttpStatus.INTERNAL_SERVER_ERROR.value(),HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase() ),null);
+    }
+
+
 }
