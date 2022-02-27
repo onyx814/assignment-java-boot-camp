f58c9cacde84b48a0a117b9971a99bef6a296328
onyx814 <love.onyx814@gmail.com>
2022-02-27 11:44:50 +0700

    feat: เพิ่มเทสการ confirm order #18
    เทสการ confirm order ในชั้น controller โดยเน้นที่การทำงาน success

---
+++ week1/src/test/java/com/example/ecommerce/member/controller/UserControllerTest.java
@@ -0,0 +1,10 @@
+package com.example.ecommerce.member.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserControllerTest {
+
+}
